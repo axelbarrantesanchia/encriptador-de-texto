172425f6b8cfc72c188f74ca36de03b1a32e8323
Axel <axelbarrantesanchia@gmail.com>
2024-08-08 17:24:19 -0600

    Cambio Proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -32,26 +32,26 @@ function App() {
     setResult(desencriptarTexto);
   };
 
-
   const Copiar = () => {
     navigator.clipboard.writeText(result).then(() => {
       alert('Texto copiado al portapapeles');
     }).catch((err) => {
       console.error('Error al copiar el texto: ', err);
+      alert('No se pudo copiar el texto. Asegúrate de que el navegador permita el acceso al portapapeles.');
     });
   };
 
   return (
     <main className="bg-black min-h-screen flex items-center justify-center">
-      <div className="flex flex-col md:flex-row p-20 rounded-lg shadow-lg w-full max-w-full">
-        <section className="md:w-1/2 p-4">
+      <div className="flex flex-col md:flex-row p-8 md:p-20 rounded-lg shadow-lg w-full max-w-4xl">
+        <section className="md:w-2/3 p-6 flex flex-col">
           <textarea
-            className="w-full h-64 p-4 border rounded"
+            className="w-full h-64 p-4 border border-gray-300 rounded bg-white text-"
             value={text}
             onChange={Cambio}
             placeholder="Ingrese el texto aquí"
           />
-          <p className="mt-2 text-gray-500">Solo letras minúsculas y sin acentos</p>
+          <p className="mt-2 text-gray-400">Solo letras minúsculas y sin acentos</p>
           <div className="flex space-x-4 mt-4">
             <button 
               onClick={Encriptar}
@@ -67,12 +67,12 @@ function App() {
             </button>
           </div>
         </section>
-        <section className="md:w-auto p-4 flex flex-col border rounded items-center bg-white justify-center ml-10 ml-40">
+        <section className="md:w-1/3 p-6 flex flex-col items-center justify-center bg-white border border-gray-300 rounded md:ml-6 mt-6 md:mt-0">
           <img src={astronauta} alt="Astronauta" className="w-40 h-40" />
-          <p className="mt-4 text-black">{result || 'Ningún mensaje fue encontrado'}</p>
+          <p className="mt-4 text-black text-center text-base">{result || 'Ningún mensaje fue encontrado'}</p>
           <button 
             onClick={Copiar} 
-            className="mt-4 px-4 py-2 border rounded text-black bg-gray-200 hover:bg-gray-300"
+            className="mt-4 px-4 py-2 border rounded text-black bg-gray-200 hover:bg-gray-300 text-base"
           >
             Copiar
           </button>
@@ -80,6 +80,7 @@ function App() {
       </div>
     </main>
   );
+  
 }
 
 export default App;
